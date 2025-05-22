8dd4350abaa36d8b479f406d85118e1bdeb17c19
Dr Joseph <josephm@backbase.com>
2025-05-22 16:51:36 +0100

    refine blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -9,7 +9,7 @@ import designMockup from '../../assets/blog/design_mockup.png';
 const Blog = () => (
   <div className="blog container">
     <p>
-      My goal in this project was to improve my use of <a href="https://www.cursor.com/" className="link">Cursor</a>, and develop my ability to write long-horizon prompts. Within the next year I think all developers will be orchestrating <a href="https://www.ibm.com/think/topics/ai-agents" className="link">Agents</a> to navigate long-horizon tasks - that is, a task that requires a series of actions and decisions - as part of our day to day work.
+      My goal in this project was to learn <a href="https://www.cursor.com/" className="link">Cursor</a>, and develop my ability to write long-horizon prompts. Within the next year I think all developers will be orchestrating <a href="https://www.ibm.com/think/topics/ai-agents" className="link">Agents</a> to navigate long-horizon tasks - that is, a task that requires a series of actions and decisions - as part of our day to day work.
     </p>
 
     <p>
@@ -17,40 +17,40 @@ const Blog = () => (
     </p>
 
     <p>
-      When I first started to utilize long-horizon prompting, I realized it wasn't an entirely familiar way of thinking about a coding challenge. Usually I would break down a task into a series of prioritized steps, and tackle each one at a time - gradually building a more complete understanding of the overall picture. But long-horizon prompting requires a comprehensive, holistic vision upfront.
+      When I first started to utilise long-horizon prompting, I realised it wasn't an entirely familiar way of thinking about a coding challenge. Usually I would break down a task into a series of prioritised steps, tackling each one at a time - gradually building a more complete understanding of the overall picture. But long-horizon prompting requires a comprehensive, holistic vision upfront.
     </p>
 
     <p>
-      And so was born the idea for a <i>one-prompt-site</i>. Not a particularly practical idea, but a fun context for learning.
+      And so the idea for a <i>one-prompt-site</i> was born. Not a particularly practical idea, but a fun context for learning.
     </p>
 
     <p>
-      After initializing a new React app, I started with coming up with a design. I sketched out what I had in mind and, continuing with a theme, had ChatGPT mock up my design so that it would be more readable moving forwards.
+      After initializing a new React app, I started by coming up with a simple sketch. Continuing with a theme, I had ChatGPT mock up my rough design so that it would be more readable moving forwards.
     </p>
     
     <img src={initialDesign} alt="Initial hand-drawn design" className="blog-image blog-image--shadowed" />
     <img src={designMockup} alt="Final design mockup" className="blog-image blog-image--shadowed blog-image--outlined" />
     
     <p>
-      Next, I wrote down anything and everything I could think of - trying to capture everything required for a <i>one-prompt-site</i>.
+      Next, I wrote down anything and everything I could think of - trying to capture everything required for a <i>one-prompt-site</i> prompt.
     </p>
 
     <p>
-      Rather than running this immediately, as suggested by <a href="https://www.prompthub.us/blog/using-llms-to-optimize-your-prompts?utm_source=chatgpt.com " className="link">Prompt Hub</a> I first ran this prompt past ChatGPT and asked if anything was missing or ambiguous. I was then able to iterate even before reaching the IDE. This, I believe, is a great practice when you're working in a context that has the Agent working for an extended period of time, as this inherently has a slow feedback loop.
+      Rather than running this prompt immediately, I first ran it past ChatGPT and asked if anything was missing or ambiguous (as suggested by <a href="https://www.prompthub.us/blog/using-llms-to-optimize-your-prompts?utm_source=chatgpt.com " className="link">Prompt Hub</a>). This led to iteration before even reaching the IDE - a great practice when working in a context that has the Agent working for an extended period of time, as this inherently has a slow feedback loop.
     </p>
     
     <p>
-      Finally I was able to get the Cursor Agent to build my site. Unfortunately, after a few alteration, I wasn't happy with the result.
+      Finally, I prompted the Agent build my site. Unfortunately, even after a few alterations, I wasn't happy with the result.
     </p>
     
     <img src={badWebsiteAttempt} alt="Bad website attempt" className="blog-image blog-image--shadowed blog-image--outlined" />
     
     <p>
-      It was at this stage that I turned to <a href="https://promptengineering.org/system-prompts-in-large-language-models/" className="link">system prompts</a>, in Cursor known as <a href="https://docs.cursor.com/context/rules" className="link">Rules</a>.
+      It was at this stage that I turned to <a href="https://promptengineering.org/system-prompts-in-large-language-models/" className="link">system prompts</a>, known as <a href="https://docs.cursor.com/context/rules" className="link">Rules</a> in Cursor.
     </p>
 
     <p>
-      These things are <i>powerful</i>. They are used to customize your Agent - building knowledge, context, and even behavior. They frame the Agent's reasoning so that it can better stick to the brief and realize your vision.
+      These things are <i>powerful</i>. They are used to customise your Agent - building knowledge, context, and even behaviour. They frame the Agent's reasoning so that it can better stick to the brief and realise your vision.
     </p>
 
     <p>
@@ -60,7 +60,7 @@ const Blog = () => (
     <img src={agentWaitingForApproval} alt="AI agent waiting for approval" className="blog-image blog-image--shadowed" />
     
     <p>
-      One stand-out for me was when it asked for more clarity around link styling. I hadn't thought to provide more information for this element previously, but it prompted me to have a think and then provide clearer instruction. I was amazed it spotted this gap and knew to ask for my input in plugging it.
+      A highlight for me was when it asked for more clarity around link styling. I hadn't given much information for this element previously, but it prompted me to have a think and then provide clearer instruction. I was amazed it spotted this gap and knew to ask for my input in plugging it.
     </p>
     
     <img src={linkStylingFeedback} alt="AI asking about link styling" className="blog-image blog-image--shadowed blog-image--centered" />
