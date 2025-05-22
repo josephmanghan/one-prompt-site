b727d146778a493868146a1129a7518ba7524931
Dr Joseph <josephm@backbase.com>
2025-05-22 19:34:11 +0100

    update app jsx with hash router

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Header from './components/Header/Header';
 import Bio from './components/Bio/Bio';
 import Contact from './components/Contact/Contact';
 import Blog from './components/Blog';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/one-prompt-site-blog" element={<Blog />} />
+				<Route path="/blog" element={<Blog />} />
 				<Route path="/*" element={
 					<Wrapper>
 						<Header />
